2e01df7d583b4e2efbc229e361f74ac3f8ebb92a
ImreZwier99 <ifrzwiers@st.noorderpoort.nl>
2024-01-29 12:38:10 +0100

    push

--- GravityFlipper/My project/Assets/Scripts/GravityChanger.cs
+++ GravityFlipper/My project/Assets/Scripts/GravityChanger.cs
@@ -3,25 +3,50 @@ using UnityEngine;
 public class GravityChanger : MonoBehaviour
 {
     public float gravity = 9.81f;
+    public float timer = 2;
+    private float timer2 = 2;
+    public bool canMove = true;
+
     private Vector2 gravityDirection = Vector2.down; // Initial gravity direction
+
+    private void Start()
+    {
+        timer2 = timer;
+    }
     private void ChangeGravityDirection()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if(canMove == true)
         {
-            gravityDirection = Vector2.up;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                gravityDirection = Vector2.up; 
+                canMove = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gravityDirection = Vector2.down;
+                canMove = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gravityDirection = Vector2.left;
+                canMove = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gravityDirection = Vector2.right;
+                canMove = false;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else
         {
-            gravityDirection = Vector2.down;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            gravityDirection = Vector2.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            gravityDirection = Vector2.right;
-        }
+            timer2 -= Time.deltaTime;
+            if (timer2 <= 0)
+            {
+                timer2 = timer;
+                canMove = true;
+            }
+        }       
     }
 
     private void ApplyGravity()
